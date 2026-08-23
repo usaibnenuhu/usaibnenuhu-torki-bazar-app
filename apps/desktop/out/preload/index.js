2a72e72df7c70718a64e386d679577743ff53099
USA IBNE NUHU <usaibnenuhu@USAs-MacBook-Pro.local>
2026-08-23 20:25:50 +0200

    Clean up generated and backup files

--- apps/desktop/out/preload/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("api", {
-  invoke: (channel, payload) => electron.ipcRenderer.invoke(channel, payload)
-});
